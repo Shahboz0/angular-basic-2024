f064aeab8ef335efddff10a8688642614883e071
Hafizov Shahboz <khafizovsf@gmail.com>
2024-05-14 19:27:14 +0300

    bindigns

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,16 +7,13 @@ import { Component } from '@angular/core';
   template: `
     <main class="main">
       <h1>{{ title }}</h1>
+      <input [value]="title">
     </main>
   `,
   styles: [`
     main {
       width: 100vw;
       height: 100vh;
-
-      display: flex;
-      justify-content: center;
-      align-items: center;
     }
   `]
 })
