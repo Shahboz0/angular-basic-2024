47816e46d8315a9fd17c904e703e6c1b51f51944
Hafizov Shahboz <khafizovsf@gmail.com>
2024-05-14 19:52:55 +0300

    ng class

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {JsonPipe, NgForOf, NgIf, NgStyle} from "@angular/common";
+import {JsonPipe, NgClass, NgForOf, NgIf, NgStyle} from "@angular/common";
 
 interface User {
   name: string;
@@ -10,16 +10,21 @@ interface User {
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, NgIf, NgForOf, JsonPipe, NgStyle],
+  imports: [FormsModule, ReactiveFormsModule, NgIf, NgForOf, JsonPipe, NgStyle, NgClass],
   template: `
     <main class="main">
       <button (click)="onClick()">Change background color</button>
-      <div *ngFor="let item of userArr" [ngStyle]="style">
+      <div *ngFor="let item of userArr" [ngClass]="show ? 'show-background' : '' ">
         <h2>{{ item | json }}</h2>
       </div>
     </main>
   `,
   styles: [`
+
+    .show-background {
+      background: yellow;
+    }
+
     main {
       width: 100vw;
       height: 100vh;
@@ -27,7 +32,7 @@ interface User {
   `]
 })
 export class MainComponent {
-  style = { 'background': 'white' }
+  show = false
   userArr: Array<User> = [
     {
       name: 'user1',
@@ -44,6 +49,6 @@ export class MainComponent {
   ]
 
   onClick(): void {
-    this.style = { 'background': 'yellow' }
+    this.show = !this.show
   }
 }
