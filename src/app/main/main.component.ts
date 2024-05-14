9d1619f288f78fbaaf5ced6a2f97fbe656cbfee4
Hafizov Shahboz <khafizovsf@gmail.com>
2024-05-14 19:40:04 +0300

    ngif directive

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, NgIf],
   template: `
     <main class="main">
+      <button (click)="onClick()">Hide text</button>
       <input type="text" [(ngModel)]="title">
-      <h1>{{ title }}</h1>
+      <h1 *ngIf="show">{{ title }}</h1>
     </main>
   `,
   styles: [`
@@ -19,5 +21,10 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
   `]
 })
 export class MainComponent {
-  title: string = ''
+  show = true;
+  title: string = '';
+
+  onClick(): void {
+    this.show = !this.show
+  }
 }
