921c74a730fe222a73efd8e6b19a0139e79d4ae4
Hafizov Shahboz <khafizovsf@gmail.com>
2024-05-15 21:40:34 +0300

    angular lifecycle

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges, OnDestroy,
+  OnInit,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-parent',
@@ -7,6 +15,43 @@ import { Component } from '@angular/core';
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.css'
 })
-export class ParentComponent {
+export class ParentComponent implements OnChanges, OnInit, DoCheck,
+  AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  constructor(){
+    console.log("constructor")
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('on changes')
+  }
+
+  ngOnInit(): void {
+    console.log('on init')
+  }
+  ngDoCheck(): void {
+    console.log('do check')
+  }
+
+  ngAfterContentInit(): void {
+    console.log('content init')
+  }
+  ngAfterContentChecked(): void {
+    console.log('content checked')
+  }
+
+
+  ngAfterViewInit(): void {
+    console.log('view init')
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('view checked')
+  }
+
+  ngOnDestroy(): void {
+    console.log('destroy')
+  }
+
 
 }
