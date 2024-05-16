11ed3f93d0a0fa2fa3a3078a121ccda4818e3ee0
Hafizov Shahboz <khafizovsf@gmail.com>
2024-05-16 20:31:06 +0300

    start with app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {ParentComponent} from "./parent/parent.component";
-import {AsyncPipe, DecimalPipe, JsonPipe, LowerCasePipe, NgIf} from "@angular/common";
-import {interval, map, Observable, Subscription} from "rxjs";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpService} from "./http.service";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, ParentComponent, NgIf, JsonPipe, LowerCasePipe, DecimalPipe, AsyncPipe, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-basic';
 
-  subscription: Subscription = new Subscription()
-
-  interval$ = new Observable<number>()
-
-  constructor(private httpClient: HttpClient) {
-
-    this.httpClient.get('https://jsonplaceholder.typicode.com/todos/').subscribe((value) => console.log(value))
+  dataInComp: any
+  constructor(private _httpService: HttpService) {
+    this.dataInComp = this._httpService.datas
   }
 }
