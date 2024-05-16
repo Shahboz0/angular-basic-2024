7042e1f3a3dd0515f92b68a5786d1b9c2864f732
Hafizov Shahboz <khafizovsf@gmail.com>
2024-05-16 21:00:29 +0300

    gen service

---
+++ src/app/communication.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {Observable, Subject} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommunicationService {
+
+  private _data$ = new Subject()
+  constructor() { }
+
+  sendData(counter: number): void {
+    this._data$.next(counter)
+  }
+
+  getData(): Observable<number | unknown> {
+    return this._data$.asObservable()
+  }
+}
