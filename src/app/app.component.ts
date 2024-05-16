16e51775e057085aacfa0c0032e9f917ac7a009e
Hafizov Shahboz <khafizovsf@gmail.com>
2024-05-16 20:07:51 +0300

    rxjs interval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ParentComponent} from "./parent/parent.component";
 import {DecimalPipe, JsonPipe, LowerCasePipe, NgIf} from "@angular/common";
+import {interval, Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,22 @@ import {DecimalPipe, JsonPipe, LowerCasePipe, NgIf} from "@angular/common";
 })
 export class AppComponent {
   title = 'angular-basic';
-  destroyComp = true
 
+  constructor() {
+    const observable = new Observable((subscriber) => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
 
-  user = {
-    id: 0,
-    name: 'User1'
+    observable.subscribe((value) => console.log(value))
+
+    const interval$ = interval(1000)
+
+    interval$.subscribe((value) => console.log('interval ', value))
   }
 }
